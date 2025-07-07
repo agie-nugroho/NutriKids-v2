f9d464ed5daad32e192f3b93ec4e77a9cea89c32
Mayy45 <pakrt556@gmail.com>
2025-07-07 09:36:29 +0700

    Terbaru
    Di merge mas

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,11 @@ const isLocalhost = window.location.hostname === "localhost";
 
 const BACKEND_URL = isLocalhost
   ? "http://localhost:3000"
-  : "https://nutrikids-v2-production-a1c4.up.railway.app";
+  : "nutrikids-v2-production-e9a1.up.railway.app";
 
 const ML_URL = isLocalhost
   ? "http://localhost:8000"
-  : "https://nutrikids-v2-production-8168.up.railway.app";
+  : "nutrikids-v2-production-c97c.up.railway.app";
 
 export const ApiBackend = axios.create({
   baseURL: BACKEND_URL,
