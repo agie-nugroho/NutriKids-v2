f768e40fe6d03d92113b57f2bf3fb23c8ad4a00b
Mayy45 <pakrt556@gmail.com>
2025-07-07 10:18:37 +0700

    nwq
    nwq

--- src/api/index.js
+++ src/api/index.js
@@ -4,11 +4,11 @@ const isLocalhost = window.location.hostname === "localhost";
 
 const BACKEND_URL = isLocalhost
   ? "http://localhost:3000"
-  : "http://nutrikids-v2-production-e9a1.up.railway.app";
+  : "https://nutrikids-v2-production-e9a1.up.railway.app";
 
 const ML_URL = isLocalhost
   ? "http://localhost:8000"
-  : "http://nutrikids-v2-production-c97c.up.railway.app";
+  : "https://nutrikids-v2-production-c97c.up.railway.app";
 
 export const ApiBackend = axios.create({
   baseURL: BACKEND_URL,
